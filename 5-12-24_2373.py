0c1c092e54d4c4ba2805acf7b43db9185d8d7562
Vladislav Vetroff <vvvetroff@vetroffmac.local>
2024-05-13 14:28:14 -0400

    Largest Local Values in a Matrix

--- 5-12-24_2373.py
+++ 5-12-24_2373.py
@@ -1,3 +1,21 @@
+'''
+2373. Largest Local Values in a Matrix
+
+You are given an n x n integer matrix grid
+
+Generate an integer matrix 'maxLocal' of size (n-2)x(n-2) such that:
+- maxLocal[i][j] is equal to the largest value of the 3x3 matrix in grid centered around row i+1 and column j+1
+
+In other words, we want to find the largest value in every contiguous 3x3 matrix in grid.
+
+Return the generated matrix.
+
+Submitted on 5-12-24, 114ms runtime (yay), O(n^2)
+
+Since each i,j pair in maxLocal represents the max of a 3x3 matrix, 
+iterate thru maxLocal, taking the max of 9 values. 
+'''
+
 class Solution:
     def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
         n = len(grid)
