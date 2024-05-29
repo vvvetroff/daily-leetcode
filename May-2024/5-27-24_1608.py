5abbab5dd62a579bc0abb85b57a50262cef7f8fd
Vladislav Vetroff <vladislav.vetroff@gmail.com>
2024-05-29 17:07:59 -0400

    Special Array With X Elements Greater Than or Equal X

--- May-2024/5-27-24_1608.py
+++ May-2024/5-27-24_1608.py
@@ -1,11 +1,17 @@
 '''
 1608. Special Array With X Elements Greater Than or Equal X
 
-You are given an array nums of non-negative integers. nums is considered special if there exists a number x such that there are exactly x numbers in nums that are greater than or equal to x.
+Difficulty: Easy
+Topics: Array, Binary Search, Sorting
+
+You are given an array nums of non-negative integers. 
+nums is considered special if there exists a number x
+such that there are exactly x numbers in nums that are greater than or equal to x.
 
 Notice that x does not have to be an element in nums.
 
-Return x if the array is special, otherwise, return -1. It can be proven that if nums is special, the value for x is unique.
+Return x if the array is special, otherwise, return -1. 
+It can be proven that if nums is special, the value for x is unique.
 
 The description left alot to be desired.
 Essentially, is there a number x such that there are x amount of 
@@ -18,14 +24,14 @@ class Solution:
     def specialArray(self, nums: List[int]) -> int:
         result = -1
         # 1st submission, O(n^2) time, O(1) space solution
-        #for i in range(len(nums)+1):
-        #    count = 0
-        #    for j in range(len(nums)):
-        #        if nums[j] >= i:
-        #            count += 1
-        #    if count == i:
-        #        result = count
-        #return result
+        for i in range(len(nums)+1):
+            count = 0
+            for j in range(len(nums)):
+                if nums[j] >= i:
+                    count += 1
+            if count == i:
+                result = count
+        return result
         
         # 2nd submission, O(n) time, O(n) space solution
         count = [0 for _ in range(len(nums)+1)]
