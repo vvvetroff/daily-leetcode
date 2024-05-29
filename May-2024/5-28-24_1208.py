8914692db4b12c96d9b8a679b89474cc0f6619e1
Vladislav Vetroff <vladislav.vetroff@gmail.com>
2024-05-29 17:09:27 -0400

    Get Equal Substrings Within Budget

--- May-2024/5-28-24_1208.py
+++ May-2024/5-28-24_1208.py
@@ -1,6 +1,9 @@
 '''
 1208. Get Equal Substrings Within Budget
 
+Difficulty: Medium
+Topics: String, Binary Search, Sliding Window, Prefix Sum
+
 You are given two strings s and t of the same length and an integer maxCost.
 
 You want to change s to t. 
@@ -9,7 +12,8 @@ Changing the ith character of s to ith character of t costs |s[i] - t[i]|
 
 Return the maximum length of a substring of s that can be changed to be the same 
 as the corresponding substring of t with a cost less than or equal to maxCost.
-If there is no substring from s that can be changed to its corresponding substring from t, return 0.
+If there is no substring from s that can be changed to its corresponding substring from t, 
+return 0.
 
 This is a sliding window problem, the first of many
 I've heard about some 'sliding window' thru some of my peers
