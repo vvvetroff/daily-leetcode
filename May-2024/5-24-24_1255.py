8860dc98e82a5fbd0a47df41ff192a6c612f89b4
Vladislav Vetroff <vladislav.vetroff@gmail.com>
2024-05-29 16:40:38 -0400

    Maximum Score Formed by Letters

--- May-2024/5-24-24_1255.py
+++ May-2024/5-24-24_1255.py
@@ -1,11 +1,18 @@
 '''
 1255. Maximum Score Words Formed by Letters
 
-Given a list of words, list of  single letters (might be repeating) and score of every character.
+Difficulty: Hard
+Topics: Array, String, Dynamic Programming
+        Backtracking, Bit Manipulation, Bit Mask
 
-Return the maximum score of any valid set of words formed by using the given letters (words[i] cannot be used two or more times).
+Given a list of words, 
+list of single letters (might be repeating) and score of every character.
 
-It is not necessary to use all characters in letters and each letter can only be used once. Score of letters 'a', 'b', 'c', ... ,'z' is given by score[0], score[1], ... , score[25] respectively.
+Return the maximum score of any valid set of words formed by using the given letters 
+(words[i] cannot be used two or more times).
+
+It is not necessary to use all characters in letters and each letter can only be used once. 
+Score of letters 'a', 'b', 'c', ... ,'z' is given by score[0], score[1], ... , score[25] respectively.
 
 Answer from Neetcode.
 I think I am beginning to understand how backtracking problems are solved.
