2e2fe6d299bc3e36899a3e33ff97df1c5fcef8ac
Vladislav Vetroff <vladislav.vetroff@gmail.com>
2024-05-29 15:25:28 -0400

    K-th Smallest Prime Fraction

--- May-2024/5-10-24_786.py
+++ May-2024/5-10-24_786.py
@@ -1,12 +1,18 @@
 '''
 786. K-th Smallest Prime Fraction
 
-You are given a sorted integer array 'arr' containing 1 and prime numbers, where all the integers of arr are unique. You are also given an integer k.
+Difficulty: Medium
+Topics: Array, Two Pointers, Binary Search, Sorting, Heap (PQ)
+
+You are given a sorted integer array 'arr' containing 1 and prime numbers, 
+where all the integers of arr are unique. You are also given an integer k.
 
 For every i and j where 0 <= i < j < arr.length, we consider the
 fraction arr[i] / arr[j].
 
-Return the kth smallest fraction considered. Return your answer as an array of integeres of size 2, where answer[0] == arr[i] and answer[1] == arr[j]
+Return the kth smallest fraction considered. 
+Return your answer as an array of integeres of size 2, 
+where answer[0] == arr[i] and answer[1] == arr[j]
 
 Generate fraction and insert the fraction and the values to make that fraction into a heap
 Extract the kth item and return it.
