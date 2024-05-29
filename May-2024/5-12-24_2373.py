965bf1aa53c751f21152f474017c97934e2ca05d
Vladislav Vetroff <vladislav.vetroff@gmail.com>
2024-05-29 15:39:59 -0400

    Largest Local Values in a Matrix

--- May-2024/5-12-24_2373.py
+++ May-2024/5-12-24_2373.py
@@ -1,10 +1,14 @@
 '''
 2373. Largest Local Values in a Matrix
 
+Difficulty: Easy
+Topics: Array, Matrix
+
 You are given an n x n integer matrix grid
 
 Generate an integer matrix 'maxLocal' of size (n-2)x(n-2) such that:
-- maxLocal[i][j] is equal to the largest value of the 3x3 matrix in grid centered around row i+1 and column j+1
+- maxLocal[i][j] is equal to the largest value of the 
+  3x3 matrix in grid centered around row i+1 and column j+1
 
 In other words, we want to find the largest value in every contiguous 3x3 matrix in grid.
 
