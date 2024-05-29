cf750e25c416d37bfbbaa21dd14fcfafb10192ac
Vladislav Vetroff <vladislav.vetroff@gmail.com>
2024-05-29 17:53:05 -0400

    Typos

--- General/746_Min-Cost-Climbing-Stairs.py
+++ General/746_Min-Cost-Climbing-Stairs.py
@@ -6,16 +6,19 @@ Topics: Array, Dynamic Programming
 
 You are given an integer array 'cost' 
 where cost[i] is the cost of ith step on a staircase.
-Once you pay the cost, you can either climb one or two steps/
+Once you pay the cost, you can either climb one or two steps.
 
 You can either start from the step with index 0,
-or the step with index 1
+or the step with index 1.
 
 Return the minimum cost to reach the top of the floor.
 
 Another DP practice question I did while in a 
-study session with Adrian. DP was the hardest topic 
-covered in our Algorithms course, so we had to get it down.
+study session with Adrian. 
+DP was the hardest topic covered in our Algorithms course, 
+so we had to get it down.
+Fortunately, most, if not all, of the DP problems were focused
+on what we knew already: Knapsack, Activity Selection, so on.
 
 This intially had a O(2^n) time,
 but with DP, its O(n) time and O(n) space
