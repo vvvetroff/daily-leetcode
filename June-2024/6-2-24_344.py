5fba7573d3be52ce2460d31af658f3e77b26a87f
Vladislav Vetroff <vladislav.vetroff@gmail.com>
2024-06-01 23:16:33 -0400

    Reverse String

---
+++ June-2024/6-2-24_344.py
@@ -0,0 +1,36 @@
+'''
+344. Reverse String
+
+Write a function that reverses a string.
+The input string is given as an array of characters s.
+
+You must do this by modifying 
+the input array in-place with O(1) extra memory.
+
+Currently on a road trip to Florida (the pan-handle)
+Using my iphone hotspot to complete this daily.
+This was kinda confusing since you can do this:
+
+    s = s[::-1]
+
+in python. ppl say it works (submission-wise)
+This is O(n) time with O(1) space
+'''
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l, r = 0, len(s)-1
+        m = l + r // 2
+        while l <= m and r >= m:
+            x = s[l]
+            y = s[r]
+            s[l] = y
+            s[r] = x
+            l += 1
+            r -= 1
+
+
+
