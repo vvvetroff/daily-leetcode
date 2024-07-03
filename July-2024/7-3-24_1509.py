284f23ebcf3718241f2586090f6d169a204643fb
Vladislav Vetroff <vladislav.vetroff@gmail.com>
2024-07-03 17:23:55 -0400

    Min Difference in Three Moves

---
+++ July-2024/7-3-24_1509.py
@@ -0,0 +1,35 @@
+'''
+1509. Minimum Difference Between Largest and Smallest
+    Value in Three Moves
+
+Difficulty: Medium
+Topics: Array, Greedy, Sorting
+
+You are given an integer array nums.
+
+In one move, you can choose one element of nums and
+change it to any value.
+
+Return the minimum difference between the largest
+and smallest value of nums after performing at most
+three moves.
+
+O(nlog)
+'''
+import math
+from typing import List
+
+
+class Solution:
+    def minDifference(self, nums: List[int]) -> int:
+        if len(nums) <= 4:
+            return 0
+
+        nums.sort()
+        result = int(math.inf)
+
+        for l in range(4):
+            r = len(nums) - 4 + l
+            result = min(result, abs(nums[r] - nums[l]))
+
+        return result
